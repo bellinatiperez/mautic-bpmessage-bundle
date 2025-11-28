ab5481da344f761c250509182aa3f060e7a9dc74
Bellinati Perez <bellinati.perez@Clebers-Mac-mini.local>
2025-11-28 11:32:11 -0300

    ajustes

--- MauticBpMessageBundle.php
+++ MauticBpMessageBundle.php
@@ -4,10 +4,7 @@ declare(strict_types=1);
 
 namespace MauticPlugin\MauticBpMessageBundle;
 
-use Mautic\CoreBundle\Helper\CoreParametersHelper;
 use Mautic\PluginBundle\Bundle\PluginBundleBase;
-use MauticPlugin\MauticBpMessageBundle\Migration\Engine;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * MauticBpMessageBundle - Integração com BpMessage API para envio de mensagens em lote.
@@ -23,49 +20,5 @@ class MauticBpMessageBundle extends PluginBundleBase
     public function getPath(): string
     {
         return __DIR__;
-    }
-
-    /**
-     * Boot the bundle and run migrations.
-     */
-    public function boot(): void
-    {
-        parent::boot();
-
-        // Run migrations on bundle boot if container is available
-        if ($this->container instanceof ContainerInterface) {
-            try {
-                $this->runMigrations();
-            } catch (\Exception $e) {
-                // Log error but don't break the application
-                if ($this->container->has('monolog.logger.mautic')) {
-                    $logger = $this->container->get('monolog.logger.mautic');
-                    $logger->error('BpMessage migrations failed: '.$e->getMessage());
-                }
-            }
-        }
-    }
-
-    /**
-     * Run plugin migrations.
-     */
-    private function runMigrations(): void
-    {
-        if (!$this->container->has('doctrine.orm.entity_manager')) {
-            return;
-        }
-
-        $entityManager = $this->container->get('doctrine.orm.entity_manager');
-        $coreParams    = $this->container->get(CoreParametersHelper::class);
-        $tablePrefix   = $coreParams->get('db_table_prefix') ?? '';
-
-        $migrationEngine = new Engine(
-            $entityManager,
-            $tablePrefix,
-            $this->getPath(),
-            'MauticBpMessageBundle'
-        );
-
-        $migrationEngine->up();
     }
 }
